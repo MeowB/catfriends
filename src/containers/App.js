acd22f42b1e630e41f4aac88460eefce08b4d0ed
MeowB <60074375+MeowB@users.noreply.github.com>
2023-05-11 11:32:23 +0200

    test more things

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
+import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css'
 
 class App extends React.Component {
@@ -35,7 +36,9 @@ class App extends React.Component {
 					<h1>CatFriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>
 					<Scroll>
-						<CardList cats={filteredCats}/>
+						<ErrorBoundry>
+							<CardList cats={filteredCats}/>
+						</ErrorBoundry>
 					</ Scroll>
 				</div>
 			)
