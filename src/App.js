13124f00705094ad398468128db09d653e1a4f06
MeowB <60074375+MeowB@users.noreply.github.com>
2023-05-11 09:40:24 +0200

    adding scrollable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css'
 
 class App extends React.Component {
@@ -34,7 +35,9 @@ class App extends React.Component {
 				<div className="tc">
 					<h1>CatFriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>
-					<CardList cats={filteredCats}/>
+					<Scroll>
+						<CardList cats={filteredCats}/>
+					</ Scroll>
 				</div>
 			)
 		}
